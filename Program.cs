c6005508b8de4b4a50b4b47554a761f131035061
Okuhle Ngada <okuhle.ngada@outlook.com>
2023-03-05 16:12:32 +0200

    wip

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿using PractiseAlgorithms.DataStructures.SinglyLinkedList;
+﻿using PractiseAlgorithms.DataStructures.BinaryTrees;
+using PractiseAlgorithms.DataStructures.SinglyLinkedList;
 using PractiseAlgorithms.Models;
 
 var person1 = new Person { Name = "Okuhle", Surname = "Ngada", Age = 18 };
 var person2 = new Person { Name = "Jack", Surname = "The Ripper", Age = 80 };
 var person3 = new Person { Name = "Naruto", Surname = "Uzumaki", Age = 30 };
+var person4 = new Person { Name = "Roxham", Surname = "Road", Age = 7 };
+var person5 = new Person { Name = "Jilly", Surname = "Jameson", Age = 13 };
 
 var linkedList = new SinglyLinkedList<Person>();
 linkedList.Add(person1);
@@ -20,4 +23,14 @@ if (findItem is null)
 else
     Console.WriteLine($"Found {person2}! The operator overloading worked!");
 
+
+ITree<Person> binarySearchTree = new BinarySearchTree<Person>();
+binarySearchTree.Insert(person1);
+binarySearchTree.Insert(person2);
+binarySearchTree.Insert(person3);
+binarySearchTree.Insert(person4);
+binarySearchTree.Insert(person5);
+
+//todo: how to iterate a binary search tree??
+//todo: how to delete items from the tree?
 Console.ReadLine();
