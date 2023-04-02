40ab7a8e96b0d20e1b147ada5e3afec6f88df770
Okuhle Ngada <okuhle.ngada@outlook.com>
2023-04-02 22:23:47 +0200

    wip

---
+++ DataStructures/Graph/GraphBase.cs
@@ -0,0 +1,13 @@
+﻿namespace PractiseAlgorithms.DataStructures.Graph;
+
+public abstract class GraphBase
+{
+    protected readonly int NumVerticies;
+    protected readonly bool IsDirected;
+
+    public GraphBase(int numVerticies, bool isDirected)
+    {
+        NumVerticies = numVerticies;
+        IsDirected = isDirected;
+    }
+}
---
+++ DataStructures/Graph/IGraph.cs
@@ -0,0 +1,15 @@
+﻿namespace PractiseAlgorithms.DataStructures.Graph;
+
+public interface IGraph<T> where T : class
+{
+    void AddItem(T item);
+    void RemoveItem(T item);
+    void AddEdge(T objectA, T objectB);
+    void RemoveEdge(T objectA, T objectB);
+    void Display();
+
+    int GetEdgeWeight();
+    int GetNumberOfVerticies();
+    List<T> GetAdjacentVerticies(T @object);
+
+}
